5067f614661b428e2c85f869d2be1fb3b8e71b98
BhavanaAngadi123 <bangadi@rivier.edu>
2026-08-19 10:00:08 -0400

    Add user repository

---
+++ src/main/java/com/happytails/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.happytails.repository;
+
+import com.happytails.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
